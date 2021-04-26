10fc8d5d0afd570b03a5c5da47118d9bd0c52591
Geo <marina_cristian.georgica@yahoo.com>
2021-04-26 11:10:32 +0300

    Update

---
+++ Pocket Tanks/Scripts/MenuScrips/MainMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+  public void PlayGame()
+  {
+      SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+  }
+
+  public void QuitGame()
+  {
+      Application.Quit();
+  }
+
+        List<int>  widths = new List<int>() {1920, 1280,960};
+    List<int>  heights = new List<int>() {1080, 800,540};
+    public void SetScreenSize(int index)
+    {
+        
+        bool fullscreen = Screen.fullScreen;
+        int width = widths[index];
+        int height = heights[index];
+        Screen.SetResolution(width, height, fullscreen);
+    }
+
+    public void SetFullScreen(bool fullscreen)
+    {
+        Screen.fullScreen = fullscreen;
+    }
+}
---
+++ Pocket Tanks/Scripts/PowerUps/PickUp.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    
+    public float fullHealth=100f;
+
+    void OnTriggerEnter (Collider other)
+    {
+        Pickup(other);
+    }
+
+    void Pickup(Collider player)
+    {
+        
+        TankHealth viata = player.GetComponent<TankHealth>();
+        viata.m_CurrentHealth = fullHealth;
+        viata.SetHealthUI();
+
+        Destroy(gameObject);
+    }
+}
